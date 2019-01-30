2f86d04bf91ed4401fc7a993f0a3e433e196e205
Filipe Correia <correia481@gmail.com>
2019-01-30 14:43:37 -0200

    primeiro commit

---
+++ src/main/java/com/example/projetoEstudo/ProjetoEstudoApplication.java
@@ -0,0 +1,14 @@
+package com.example.projetoEstudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstudoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/projetoEstudo/ProjetoEstudoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.projetoEstudo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoEstudoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
