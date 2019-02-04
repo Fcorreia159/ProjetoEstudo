997738b42affb309806873fd23d7d59c7d70a4b3
Filipe Correia <correia481@gmail.com>
2019-02-04 15:32:12 -0200

    Estado e Cidade

--- src/main/java/com/example/projetoEstudo/ProjetoEstudoApplication.java
+++ src/main/java/com/example/projetoEstudo/ProjetoEstudoApplication.java
@@ -10,8 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.projetoEstudo.domain.Categoria;
+import com.example.projetoEstudo.domain.Cidade;
+import com.example.projetoEstudo.domain.Estado;
 import com.example.projetoEstudo.domain.Produto;
 import com.example.projetoEstudo.repositories.CategoriaRepository;
+import com.example.projetoEstudo.repositories.CidadeRepository;
+import com.example.projetoEstudo.repositories.EstadoRepository;
 import com.example.projetoEstudo.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -22,6 +26,12 @@ public class ProjetoEstudoApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoEstudoApplication.class, args);
@@ -47,9 +57,30 @@ public class ProjetoEstudoApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+		
+		
+		
+		
+	
+		
 		
 		
 		
